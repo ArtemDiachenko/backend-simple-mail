f832f35f5e2026afff58498442c617f1890f8f66
Artem Diachenko <artem.selesta@gmail.com>
2023-04-01 21:53:37 +0300

    Start

---
+++ src/main/java/com/simplemail/smail/SmailApplication.java
@@ -0,0 +1,13 @@
+package com.simplemail.smail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplemail/smail/SmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplemail.smail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
